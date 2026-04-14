3b84861162e29844b3905d3378c749a49eaca3a2
seacoral <247729792+seacoral1223@users.noreply.github.com>
2026-04-14 17:24:41 +0800

    服务器

--- myserver.py
+++ myserver.py
@@ -8,31 +8,24 @@ class MyRequestHandler(BaseHTTPRequestHandler):
         self.send_header('Content-Type', 'application/json; charset=utf-8')  
         #关闭头
         self.end_headers()
-
-        res = ("你好吗？")
-
+        res={"message": "你好吗？"}
+        # res = ("你好吗？")
         self.wfile.write(json.dumps(res).encode("utf-8"))
         # self.wfile.write(res.encode("utf-8"))
-
     def do_POST(self):
         content_text_size = int(self.headers['Content-Length'])
         data = self.rfile.read(content_text_size)
-
         try:
             tran_data = json.loads(data.decode("utf-8"))
             print("接收到的数据：", tran_data)
-
             self.send_response(200)
-
             self.send_header('Content-Type', 'application/json; charset=utf-8')
-
             self.end_headers()
-            res = "我已收到数据"
+            res = {"message": "我已收到数据"}
+            # res = ("我已收到数据")
             self.wfile.write(json.dumps(res).encode("utf-8"))
             # self.wfile.write(res.encode("utf-8"))
         except Exception as e:
             print("数据解析失败：", e)
-
-
 http = HTTPServer(server_address=('', 8001), RequestHandlerClass=MyRequestHandler).serve_forever()
 
